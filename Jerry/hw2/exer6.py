4f4d47442d740e4f59a800f6425c67c5663d8c54
Moelf <jerryling315@gmail.com>
2019-01-24 13:29:55 -0800

    Faster prime factorization

--- Jerry/hw2/exer6.py
+++ Jerry/hw2/exer6.py
@@ -2,40 +2,28 @@ import math
 
 #this helper function returns primitivity
 def isPrime(num):
-    flag = True
     for k in range(2,int(math.sqrt(num)+1)):
         if num%k == 0:
-            flag = False
-            break
-    return flag
-
-#this helper function will list primes in range 2 to upper
-def findPrimes(upper):
-    rel = []
-    for i in range(2,upper+1):
-        if isPrime(i): rel.append(i)
-    return rel
+            return False
+    return True
 
 try:
-    largeNum = int(input("Please just give me a normal >1 number to do prime factoriztaion: "))
+    largeNum = abs(int(input("Please just give me a normal >1 number to do prime factoriztaion: ")))
 except:
     print("Sorry that's not an integer")
+if largeNum <= 1: print("Sorry 0 or 1 won't work")
 
-largeNum = abs(largeNum)
-if largeNum == 1: print("Sorry 1 won't work")
-primeList = findPrimes(largeNum)
+i=2
 countDict = dict()
-
-
-power = 0
-for i in primeList:
-    if largeNum % i != 0: continue
-    while largeNum % i == 0:
-        power+=1
-        largeNum /= i
-    if power > 0:
-        countDict[i] = power
-        power = 0
+while largeNum > 1:
+    while largeNum%i == 0:
+        if isPrime(i):
+            largeNum/=i
+            if i in countDict.keys():
+                countDict[ i ] += 1
+            else:
+                countDict[ i ] = 1
+    i+=1
 
 for key in countDict.keys():
     print(key, "to the power of", countDict[key])
