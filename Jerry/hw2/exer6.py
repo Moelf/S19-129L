7eea83b136771cd6a999840f7f0ba3d7db5eb886
Moelf <jerryling315@gmail.com>
2019-01-24 16:23:06 -0800

    Final commit for hw2, submit

--- Jerry/hw2/exer6.py
+++ Jerry/hw2/exer6.py
@@ -1,20 +1,9 @@
-# import math
-# import time
-
-#this helper function returns primitivity
-# def isPrime(num):
-#     for k in range(2,int(math.sqrt(num)+1)):
-#         if num%k == 0:
-#             return False
-#     return True
-
 try:
     largeNum = abs(int(input("Please just give me a normal >1 number to do prime factoriztaion: ")))
 except:
     print("Sorry that's not an integer")
 if largeNum <= 1: print("Sorry 0 or 1 won't work.")
 
-# start_time = time.time()
 i=2
 countDict = dict()
 while largeNum > 1:
@@ -28,5 +17,3 @@ while largeNum > 1:
 
 for key in countDict.keys():
     print(key, "to the power of", countDict[key])
-
-# print("--- %s seconds ---" % (time.time() - start_time))
