6d80336a83d99b7febdc8d3473910aed80381cf0
Moelf <jerryling315@gmail.com>
2019-01-31 10:50:00 -0800

    Last commit for hw2

--- Jerry/Ling-4375473-hw2/exer6.py
+++ Jerry/Ling-4375473-hw2/exer6.py
@@ -1,19 +1,21 @@
+import time
 try:
     largeNum = abs(int(input("Please just give me a normal >1 number to do prime factoriztaion: ")))
 except:
     print("Sorry that's not an integer")
 if largeNum <= 1: print("Sorry 0 or 1 won't work.")
 
+start = time.time()
 i=2
-countDict = dict()
+# countDict = dict()
+res = []
 while largeNum > 1:
     while largeNum%i == 0:
         largeNum/=i
-        if i in countDict.keys():
-            countDict[ i ] += 1
-        else:
-            countDict[ i ] = 1
+        res.append(i)
     i+=1
-
-for key in countDict.keys():
-    print(key, "to the power of", countDict[key])
+timeer = time.time() - start
+print("%s seconds" %(timeer))
+# for key in countDict.keys():
+#     print(key, "to the power of", countDict[key])
+print(res)
