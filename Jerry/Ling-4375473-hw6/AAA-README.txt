d5500aac48c9fb176a87d47a3bb0e244e739079a
Moelf <jerryling315@gmail.com>
2019-02-27 12:46:29 -0800

    Draf of hw6

---
+++ Jerry/Ling-4375473-hw6/AAA-README.txt
@@ -0,0 +1,10 @@
+Using python 3.5.3
+list of modules and their versions can be found in module_list.txt
+
+*** For all problems, just run 'python exer*.py' where * is the problem number. ***
+Required outputs should be print to the terminal emulator, or draw to graphical
+output through matplotlib, or through interaction prompt.
+
+P.S:
+./LVector.py contains a class called LVector for exer1, you can run 'python ./LVector_specification.py' to check it works
+./ccHistStuff.py contains Claudio's stat box class to be used for problems
---
+++ Jerry/Ling-4375473-hw6/exer3.py
@@ -0,0 +1,31 @@
+import numpy as np
+import math
+
+# constants given in the problem
+N = int(input("N = "))
+mu = int(input("mu = "))
+sigma = float(input("sigma = "))
+n = 10**4
+
+# list of S to scan
+Ss = np.linspace(0, max(10, N+np.sqrt(N)), 1000)
+# generate a B for each S, out n is large enough
+Bs = np.random.normal(mu, sigma, len(Ss))
+try:
+    Bs = Bs[Bs >= 0][:len(Ss)]
+except IndexError:
+    print("not enough B's, try again")
+
+# list of len(Ss)=3000 elements, each is an tuple of
+# (array of n=10^5 possion pick, S)
+S_p_B = [(np.random.poisson(Ss[i]+Bs[i], n), Ss[i]) for i in range(0, len(Ss))]
+
+# initialize a large number as upper limit
+smallS = math.inf
+
+for tup in S_p_B:
+    # update smallS if less than 5% of n<N
+    if sum(tup[0] < N) < (0.05 * len(Ss)):
+        smallS = tup[1] if (tup[1] < smallS) else smallS
+
+print("95% CL excluding signal >= {:.2f}".format(smallS))
