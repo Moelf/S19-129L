a7d55222e7e4ea15bd24ff0223bb3d5c4768d6bf
zlewis <zacharyjlewis98@gmail.com>
2019-03-01 12:36:14 -0800

    more work on hw7

--- Zach/hw7/AAA-README.txt
+++ Zach/hw7/AAA-README.txt
@@ -1,23 +1,20 @@
 Problem 1:
 -----------
 Run ex1.py
+Gives two histograms. The first one has both X1-X0 and X2-X0 on the same graph
+while the second has Xav-X0.
 
 Problem 2:
 -----------
 Run ex2.py
+Constructed a chi-squared regression according to an exponential function using
+Campagnari's definition and procedure outlined in the problem and notes. Superimposes
+the fitted function on the histogram of the raw data and prints the parameters P0 and P1.
 
 Problem 3:
 -----------
 Run ex3.py
+Similar to problem 1, but fits the two tracks simultaneously and calculates the pull
+as well. Plots Xf-X0 and then the pull (Xf-X0)/Xf also.
 
-Problem 4:
------------
-Run ex4.py
 
-Problem 5:
------------
-Run ex5.py
-
-Problem 6:
------------
-Run ex6.py
--- Zach/hw7/ex3.py
+++ Zach/hw7/ex3.py
@@ -1,4 +1,71 @@
 #!/usr/bin/env python3
 
+import numpy as np
+import scipy.stats
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import ccHistStuff as cc
 
+data=np.loadtxt("straightTracks.txt")
+
+#Positions in cm
+x1 = 2 
+x2 = 3
+x3 = 5
+x4 = 7
+difference  = []
+pull_values = []
+
+def func(x,a,b):
+	return a*x+b
+
+for i in range(len(data)):
+	x0  = data[i][0]
+	y0  = data[i][1]
+	y00 = data[i][2]
+	y01 = data[i][3]
+	y02 = data[i][4]
+	y03 = data[i][5]
+	y10 = data[i][6]
+	y11 = data[i][7]
+	y12 = data[i][8]
+	y13 = data[i][9]
+
+	x_data  = [x0,x1, x2, x3, x4]
+	y0_data = [y0, y00, y01, y02, y03]
+	y1_data = [y0, y10, y11, y12, y13]
+
+	x_data  = np.hstack([x_data,x_data])
+	y_data  = np.hstack([y0_data,y1_data])
+
+	popt, pcov = curve_fit(func,x_data,y_data)
+	intercept  = popt[1]
+	diff       = intercept - x0
+	difference.append(diff)
+
+	pull = diff/np.diag(pcov)[1]
+	pull_values.append(pull)
+	
+#Plotting
+fig, ax = plt.subplots()
+
+binEdges = np.linspace(-.05,.05,100)
+
+ax.hist(difference, bins=binEdges, label='Difference', edgecolor='black')
+cc.statBox(ax, difference, binEdges, label='Constrained Fit')
+plt.legend(loc='center right')
+plt.xlabel('Intercept Difference [cm]')
+plt.ylabel('Number of Occurences')
+fig.show()
+input('Press <Enter> to continue')
+
+fig, ax = plt.subplots()
+binEdges = np.linspace(-.05,.05,100)
+ax.hist(pull_values, bins=binEdges, label='Pull', edgecolor='black')
+cc.statBox(ax, pull_values, binEdges, label='Pull')
+plt.legend(loc='center right')
+plt.xlabel('Pull')
+plt.ylabel('Number of Occurences')
+fig.show()
+input('Press <Enter> to continue')
 
