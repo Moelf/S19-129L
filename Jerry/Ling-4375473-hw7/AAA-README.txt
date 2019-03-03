11f034bc499d6968327de4575a38423e1d572a99
Moelf <jerryling315@gmail.com>
2019-03-02 22:08:12 -0800

    Test fullwindw display

---
+++ Jerry/Ling-4375473-hw7/AAA-README.txt
@@ -0,0 +1,12 @@
+Using python 3.5.3
+list of modules and their versions can be found in module_list.txt
+
+*** For all problems, just run 'python exer*.py' where * is the problem number. ***
+Required outputs should be print to the terminal emulator, or draw to graphical
+output through matplotlib, or through interaction prompt.
+
+exer1:
+Be advised that the histogram's visual is best displayed at full screen due to label overlap handling
+
+P.S:
+./ccHistStuff.py contains Claudio's stat box class 
--- Jerry/Ling-4375473-hw7/exer1.py
+++ Jerry/Ling-4375473-hw7/exer1.py
@@ -9,19 +9,20 @@ import matplotlib.pyplot as plt
 # ---------------------------------
 
 
-def statBox(ax, entries, binEdges, x=0.96, y=0.98, fontsize='medium'):
+def statBox(ax, entries, binEdges, x=0.96, y=0.98, fontsize='medium', name='', y_shift=0):
     """
     Put a stat box on the histogram at coord x and y
     font = medium is appropriate for 1x1.  Other choices are
     size in points, 'xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large'
     """
+    y = y+y_shift
     en = len(entries)                   # number of entries
     ov = (entries > binEdges[-1]).sum()   # overflows
     uf = (entries < binEdges[0]).sum()    # underflows
     mn = entries.mean()                 # mean
     sd = entries.std()                  # standard deviation
-    textstr = 'N=%i \nOverflow=%i \nUnderflow=%i \n$\mu=%.2f$ \n$\sigma=%.2f$' % (
-        en, ov, uf, mn, sd)
+    textstr = '%s\n N=%i \nOverflow=%i \nUnderflow=%i \n$\mu=%.2f$ \n$\sigma=%.2f$' % (name,
+                                                                                       en, ov, uf, mn, sd)
     props = dict(boxstyle='round', facecolor='white', alpha=0.5)
     ax.text(x, y, textstr,
             transform=ax.transAxes,
@@ -34,18 +35,54 @@ def statBox(ax, entries, binEdges, x=0.96, y=0.98, fontsize='medium'):
 # ---------------------------------
 
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(nrows=2)
 # import binary data set
-raw = np.loadtxt("./straightTracks.txt")
-X0=raw[:,0]
-Y0=raw[:,1]
-# # set up bins and scale after observation
-# binEdges = np.linspace(100, 200, 25)
-# ax.hist(x, bins=binEdges, align="left")
-# # label everything
-# plt.xlabel("Value")
-# plt.ylabel("Events count")
-# # add claudio's box
-# statBox(ax, x, binEdges)
-# plt.title("Extraction of text file containing $e^+ e^-$ invar mass")
-# plt.show()
+# this has shape (1000,10)
+raw = np.array(np.loadtxt("./straightTracks.txt"))
+# x positions of pixel layer
+layer_x = np.array([2, 3, 5, 7], dtype=float)
+
+# set up array
+histX1X0 = np.empty(len(raw))
+histX2X0 = np.empty(len(raw))
+histXav = np.empty(len(raw))
+for i, data in enumerate(raw):
+    # position in the length 10 line
+    ytrack1 = data[2:6]
+    ytrack2 = data[6:]
+    X0 = data[0]
+
+    # [1] in fit is the constant term correspond to y-axis intersect
+    X1 = np.polyfit(layer_x, ytrack1, 1)[1]
+    X2 = np.polyfit(layer_x, ytrack2, 1)[1]
+    Xav = (X1+X2)/2 - X0
+
+    histX1X0[i] = X1-X0
+    histX2X0[i] = X2-X0
+    histXav[i] = Xav
+
+# convert to micrometer
+histX1X0 *= 10**4
+histX2X0 *= 10**4
+histXav *= 10**4
+
+# set up bins and scale after observation
+binEdges = np.linspace(-500, 500, 100)
+ax[0].hist([histX1X0, histX2X0], bins=binEdges,
+           align="left", label=["X1X0", "X2X0"])
+ax[0].legend(loc="upper left")
+ax[1].hist(histXav, bins=binEdges, align="left")
+# label everything
+ax[0].set_title("(X1 or X2) - X0")
+ax[1].set_title("Xav - X0")
+ax[0].set(xlabel="$\Delta$ X ($\mu m$)")
+ax[1].set(xlabel="$\Delta$ X ($\mu m$)")
+# add claudio's box
+statBox(ax[0], histX1X0, binEdges, name="X1X0")
+statBox(ax[0], histX2X0, binEdges, name="X2X0", y_shift=-0.3)
+statBox(ax[1], histXav, binEdges)
+
+# maxmize window
+mng = plt.get_current_fig_manager()
+mng.full_screen_toggle()
+plt.show()
