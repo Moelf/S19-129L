fb1410e86e454ef5dae8c2d5fc54c014414138c4
Moelf <jerryling315@gmail.com>
2019-03-14 10:45:13 -0700

    Update for exer2

--- Jerry/Ling-4375473-hw8/exer2.py
+++ Jerry/Ling-4375473-hw8/exer2.py
@@ -1,4 +1,5 @@
 import pickle
+import math
 from math import pi
 import numpy as np
 import matplotlib.pyplot as plt
@@ -51,8 +52,8 @@ def EoverC(p, m):
 
 # get a 3 velocity a 4-momentum, for boost
 def PtoV(p):
-    p3 = p[1:]
-    v = p3/float(p[0])
+    p3 = p.get_r()
+    v = p3/p.v[0]
     return v
 
 
@@ -66,6 +67,16 @@ def restDecayMomentum(M, m1, m2):
 def decayAngle(v1, v2):
     denom = np.sqrt(np.dot(v1, v1) * np.dot(v2, v2))
     return np.arccos(np.dot(v1, v2) / denom)
+
+
+# R = x^3 -> x = (R)1/3
+def SpinTheta():
+    R = np.random.rand()
+    theta = np.arccos((R)**(1/3))
+    if np.random.rand() < 0.5:
+        return -theta + pi/2
+    else:
+        return theta + pi/2
 
 
 def restSpinLessDecay(M, m1, m2):
@@ -83,24 +94,47 @@ def restSpinLessDecay(M, m1, m2):
     return lvp1, lvp2
 
 
+def restSpinDecay(M, m1, m2):
+    p = restDecayMomentum(M, m1, m2)
+    # this account for the Wigner angle but in it's rest frame
+    theta1 = SpinTheta()
+    phi1 = randomPhi()
+    # 3-momentums
+    p1 = p*spheToCartesian(phi1, theta1)
+    p2 = -p1
+
+    p1 = np.insert(p1, 0, EoverC(p1, m1))
+    p2 = np.insert(p2, 0, EoverC(p2, m2))
+    # NOW 4-momentums
+    lvp1 = Lv(p1)
+    lvp2 = Lv(p2)
+    return lvp1, lvp2
+
+
 if __name__ == "__main__":
     output = []
     heli = []
     for _ in range(1000):
         pDstar0, pPi_p1 = restSpinLessDecay(massB_p, massD_star0, massPi_p)
+        thetaDstar0, phiDstar0 = pDstar0.theta(), pDstar0.phi()
+        axis = np.cross(pDstar0.get_r(), [0, 0, 1])
+        angle = -np.arcsin(np.linalg.norm(axis)/pDstar0.get_rlength())
 
         # this is spin 1 decay
-        pD0, pPi0 = restSpinLessDecay(massD_star0, massD_0, massPi_0)
-        pD0.boost(PtoV(pDstar0.v))
-        pPi0.boost(PtoV(pDstar0.v))
+        pD0, pPi0 = restSpinDecay(massD_star0, massD_0, massPi_0)
+        roteMatrix = pD0.rotate_by_axis(axis, angle)
+        print(pD0.v)
+        pPi0.rotate_by_matrix(roteMatrix)
+        pD0.boost(PtoV(pDstar0))
+        pPi0.boost(PtoV(pDstar0))
 
         pK_m, pPi_p2 = restSpinLessDecay(massD_0, massK_m, massPi_p)
-        pK_m.boost(PtoV(pD0.v))
-        pPi_p2.boost(PtoV(pD0.v))
+        pK_m.boost(PtoV(pD0))
+        pPi_p2.boost(PtoV(pD0))
 
         pGamma1, pGamma2 = restSpinLessDecay(massPi_0, 0, 0)
-        pGamma1.boost(PtoV(pPi0.v))
-        pGamma2.boost(PtoV(pPi0.v))
+        pGamma1.boost(PtoV(pPi0))
+        pGamma2.boost(PtoV(pPi0))
 
         output.append([pPi_p1, pK_m, pPi_p2, pGamma1, pGamma2])
 
