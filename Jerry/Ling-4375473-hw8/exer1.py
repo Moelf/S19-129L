fd52742560041c6332f54c9da4aa197af308bbc6
Moelf <jerryling315@gmail.com>
2019-03-21 13:34:00 -0700

    Finish hw8

--- Jerry/Ling-4375473-hw8/exer1.py
+++ Jerry/Ling-4375473-hw8/exer1.py
@@ -71,14 +71,14 @@ def NLL2(S, B, a, b, c):
 
 
 # appropriate NLLS to minimize
-m0 = Minuit(NLL0, S=20., B=180., a=0.5, errordef=0.5, print_level=0,
+m0 = Minuit(NLL0, S=30., B=170., a=0.5, errordef=0.5, print_level=0,
             error_S=1.0, error_B=1.0, error_a=0.1)
-m1 = Minuit(NLL1, S=20., B=180., a=10., b=2, errordef=0.5, print_level=0,
-            error_S=1.0, error_B=1.0, error_a=0.4, error_b=0.1)
-m2 = Minuit(NLL2, S=20., B=180., a=0.001, b=-300, c=-30, print_level=0,
+m1 = Minuit(NLL1, S=30., B=170., a=900., b=0.5, errordef=0.5, print_level=0,
+            error_S=1.0, error_B=1.0, error_a=0.1, error_b=0.1)
+m2 = Minuit(NLL2, S=30., B=170., a=0.01, b=-300, c=+25000, print_level=0,
             errordef=0.5, error_S=1.0, error_B=1.0, error_a=0.1, error_b=0.1, error_c=0.1)
 
-# so hakcy I hate it, if your function can't be pickle serilized
+# hakcy, I hate it, turns out if the function can't be pickle serilized
 # multiprocessing cannot infer the environment, a work around is to bring
 # everything back to global scope
 mList = [m0, m1, m2]
@@ -87,13 +87,13 @@ errs = []
 
 
 def runm(i):
-    global all_ys, xs, scale
+    global all_ys, xs, scale, S_s
     print("Minimization # {} started.".format(i+1))
-    fmin, param = mList[i].migrad()
+    fmin, param = mList[i].migrad(nsplit=2)
     mList[i].minos()
-    print("Minimization # {} finished.".format(i+1))
 
     S, S_error = param[0]['value'], param[0]['error']
+    print("Minimization # {0} finished, best S = {1}".format(i+1,S))
     errs.append(S_error)
     B = param[1]['value']
     arguments = [x['value'] for x in param[2:]]
@@ -107,34 +107,31 @@ def runm(i):
     return ys*histArea/(S+B)
 
 
+# parallel run
 pool = Pool(processes=3)
 all_ys = pool.map(runm, [0, 1, 2])
 f, ax = plt.subplots(2, 3)
-for i in range(3):
+# do all the dawrings
+for i in [0, 1, 2]:
     ax[0, i].plot(xs, all_ys[i])
     ax[0, i].hist(measured, bins=binEdges, histtype='step')
-    xxx, yyy, _ = mList[i].mnprofile('S', subtract_min=True, bins=25, bound=(0.1, 40))
+    xxx, yyy, _ = mList[i].mnprofile(
+        'S', subtract_min=True, bins=15, bound=(0.1, 40))
 
-    ax[1,i].plot(xxx, yyy, linestyle='solid', color='b')
-    ax[1,i].set_xlim(min(xxx), max(xxx))
-    ax[1,i].set_ylim(0.)
-    ax[1,i].set_xlabel('S')
-    ax[1,i].set_ylabel('deltaNLL')
-    ax[1,i].plot([min(xxx), max(xxx)], [0.5, 0.5], linestyle='dashed', color='red')
-    ax[1,i].plot([min(xxx), max(xxx)], [2.0, 2.0], linestyle='dashed', color='red')
-    ax[1,i].plot([min(xxx), max(xxx)], [4.5, 4.5], linestyle='dashed', color='red')
+    ax[1, i].plot(xxx, yyy, linestyle='solid', color='b')
+    ax[1, i].set_xlim(min(xxx), max(xxx))
+    ax[1, i].set_ylim(0.)
+    ax[1, i].set_xlabel('S')
+    ax[1, i].set_ylabel('deltaNLL')
+    ax[1, i].plot([min(xxx), max(xxx)], [0.5, 0.5],
+                  linestyle='dashed', color='red')
+    ax[1, i].plot([min(xxx), max(xxx)], [2.0, 2.0],
+                  linestyle='dashed', color='red')
+    ax[1, i].plot([min(xxx), max(xxx)], [4.5, 4.5],
+                  linestyle='dashed', color='red')
 
-
-# print(np.shape(all_ys))
+# do all the titles
+ax[0,0].set_title("Exponential bkg,\nS = {}".format(22.55))
+ax[0,1].set_title("Power law bkg, \nS = {}".format(23.49))
+ax[0,2].set_title("Quadratic bkg, \nS = {}".format(18.63))
 plt.show()
-# # import binary data set
-# # set up bins and scale after observation
-# fig, ax = plt.subplots()
-# ax.hist(measured, bins=binEdges, align="left", histtype="step")
-# # label everything
-# plt.xlabel("Value")
-# plt.ylabel("Events count")
-# # add claudio's box
-# statBox(ax, measured, binEdges)
-# plt.title("Extraction of text file containing $e^+ e^-$ invar mass")
-# plt.show()
